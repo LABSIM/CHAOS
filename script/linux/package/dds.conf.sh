f35bccd344d93aede81a1666ceb83992c1688e8b
Nawfel Kinani <nawfel.kinani@onera.fr>
2014-04-09 12:04:00 +0000

    [dev] correction d'une option de la v2

--- script/linux/package/dds.conf.sh
+++ script/linux/package/dds.conf.sh
@@ -18,7 +18,7 @@ RHEA_ROOT=$DEV_HOME/Titans/RHEA/$DEV_SUBDIR
 
 export RHEA_ROOT
 
-alias rhea="perl $RHEA_ROOT/bin/v2/RHEA.pl"
+alias rhea="perl -I $RHEA_ROOT/etc/modules $RHEA_ROOT/bin/v2/RHEA.pl"
 
 ###################################################################
 # KRONOS-DDS 
