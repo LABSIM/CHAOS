d93f53038db369815b6eaf9831d4495e1dd75d7b
Nawfel Kinani <nawfel.kinani@onera.fr>
2015-09-07 15:18:33 +0000

    [dev] Ajout de Silverlining et Triton

---
+++ bin/linux/install/install_Triton.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# get current directory
+current_dir=$PWD
+
+# source user configured versions 
+source user-config-versions.sh
+
+if [ $(whoami) == "root" ] 
+then
+  echo
+  echo "##############################################"
+  echo " Vous NE devez PLUS etre root pour executer ce script :)"
+  echo "##############################################"
+  echo
+  exit
+fi
+
+# go to dir
+cd /home/$(whoami)/tritoninst
+
+echo; echo "## BEGIN"; echo
+
+echo "########################################################"
+echo "  FFTS : Decompression du/des package(s)"
+echo "########################################################"
+
+echo "#!/bin/bash" > exec.sh
+echo "tar -xzvf /data/CentOS_6.3/archive/fftss-$FFTSS_INSTALL_TARGET_VERSION.tar.gz" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tDecompression de FFTSS $FFTSS_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+echo "########################################################"
+echo "  FFTS : Configure, build & install"
+echo "########################################################"
+
+cd fftss-*
+
+echo "#!/bin/bash" > exec.sh
+echo "./configure --prefix=/home/$(whoami)/Progiciels/fftss-$FFTSS_INSTALL_TARGET_VERSION --enable-shared" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tConfiguration de FFTSS $FFTSS_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make -j4" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tCompilation de FFTSS $FFTSS_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make install" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tInstallation de FFTSS $FFTSS_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+cd ../triton-sdk-fullsource
+
+echo "########################################################"
+echo "  Triton : CMake, Build & Install"
+echo "########################################################"
+
+echo "#!/bin/bash" > exec.sh
+echo "cmake -DCMAKE_INSTALL_PREFIX=/home/$(whoami)/Progiciels/triton-$TRITON_INSTALL_TARGET_VERSION -DFFTSS_INCLUDE_DIR=/home/$(whoami)/Progiciels/fftss-$FFTSS_INSTALL_TARGET_VERSION/include -DFFTSS_LIBRARY=/home/$(whoami)/Progiciels/fftss-$FFTSS_INSTALL_TARGET_VERSION/lib/libfftss.so ." >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tConfiguration du SDK Triton $TRITON_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make -j4" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tBuild du SDK Triton $TRITON_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make install" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tInstalation du SDK Triton $TRITON_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --disable-factory --working-directory $PWD --command "./exec.sh" --window
+echo "OK"
+
+echo "########################################################"
+echo "  Suppression du repertoire temporaire"
+cd $current_dir
+rm -rf /home/$(whoami)/tritoninst
+echo "########################################################"
+
+echo; echo "## END"; echo
