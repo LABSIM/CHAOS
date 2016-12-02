a6d8317d070ea190eb144b1c356b3c4f3f717a32
Nawfel Kinani <nawfel.kinani@onera.fr>
2016-12-02 10:38:58 +0000

    [dev] Ajout des codes necessaires en prepa de Thetys V2 et du nouveau protocole de streaming GAIA

--- INSTALL-linux-CentOS-6.x
+++ INSTALL-linux-CentOS-6.x
@@ -12,6 +12,8 @@ I - Package (YUM)
 II - Libraries
 ----------------------------------------------------------------------------
 
+	// Common section
+	
 	-> script/linux/install/CentOS-6.x/install_PERL.sh	
 	-> script/linux/install/CentOS-6.x/install_CPAN.sh
 	-> script/linux/install/CentOS-6.x/install_CMAKE.sh
@@ -21,16 +23,34 @@ II - Libraries
 	-> script/linux/install/CentOS-6.x/install_GLFW.sh
 	-> script/linux/install/CentOS-6.x/install_QT.sh
 	-> script/linux/install/CentOS-6.x/install_BOOST.sh
+	
+	// Graphic section
+	
 	-> script/linux/install/CentOS-6.x/install_OSG.sh
 	-> script/linux/install/CentOS-6.x/install_SILVERLINING.sh
 	-> script/linux/install/CentOS-6.x/install_TRITON.sh
+	
+	// VR section
+	
 	-> script/linux/install/CentOS-6.x/install_OPENCV.sh
 	-> script/linux/install/CentOS-6.x/install_HIDAPI.sh
 	-> script/linux/install/CentOS-6.x/install_JSONCPP.sh
 	-> script/linux/install/CentOS-6.x/install_OSVR_LIBFUNCTIONALITY.sh
 	-> script/linux/install/CentOS-6.x/install_OSVR_CORE.sh
 	-> script/linux/install/CentOS-6.x/install_OSVR_TRACKER.sh
+	
+	// DDS section 
+	
 	-> script/linux/install/CentOS-6.x/install_OSPL.sh
+	
+	// Google section
+	
+	-> script/linux/install/CentOS-6.x/install_GO.sh
+	-> script/linux/install/CentOS-6.x/install_PROTOBUF.sh
+	-> script/linux/install/CentOS-6.x/install_GRPC.sh
+	
+	// CEGUI (depreciated?)
+	
 	-> script/linux/install/CentOS-6.x/install_CEGUI.sh
 
 ----------------------------------------------------------------------------
---
+++ script/linux/install/CentOS-6.x/install_PROTOBUF.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+#
+# GAIA : required LABSIM ground software ecosystem
+# Copyright (C) 2012-2016  Nawfel KINANI
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.
+# If not, see <http://www.gnu.org/licenses/>.
+#
+
+# get current directory
+current_dir=$PWD
+
+# source user configured versions 
+source user-config-versions.sh
+source ../../routine/wait_for_PID.sh
+
+# go to local dir
+cd /tmp_user/$(hostname)/$(whoami)
+mkdir protobufinst
+cd protobufinst
+
+echo; echo "## BEGIN"; echo
+
+echo "########################################################"
+echo "                      	PROTOBUF                    "
+echo "########################################################"
+
+echo "#!/bin/bash" > exec.sh
+echo "git clone --recursive https://github.com/google/protobuf.git" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tCloning de Protobuf $PROTOBUF_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --working-directory $PWD --title="LABSIM - Protobuf $PROTOBUF_INSTALL_TARGET_VERSION" --command "./exec.sh" --window
+sleep 0.2
+PID=$(pgrep exec.sh)
+wait_for_PID $PID
+echo "OK"
+
+cd protobuf*
+
+echo "#!/bin/bash" > exec.sh
+echo "./autogen.sh" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tGeneration du fichier de configuration pour Protobuf $PROTOBUF_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --working-directory $PWD --title="LABSIM - Protobuf $PROTOBUF_INSTALL_TARGET_VERSION" --command "./exec.sh" --window
+sleep 0.2
+PID=$(pgrep exec.sh)
+wait_for_PID $PID
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "./configure --prefix=/home/$(whoami)/Progiciels/protobuf-$PROTOBUF_INSTALL_TARGET_VERSION" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tConfiguration de Protobuf $PROTOBUF_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --working-directory $PWD --title="LABSIM - Protobuf $PROTOBUF_INSTALL_TARGET_VERSION" --command "./exec.sh" --window
+sleep 0.2
+PID=$(pgrep exec.sh)
+wait_for_PID $PID
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make -j4" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tBuild de Protobuf $PROTOBUF_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --working-directory $PWD --title="LABSIM - Protobuf $PROTOBUF_INSTALL_TARGET_VERSION" --command "./exec.sh" --window
+sleep 0.2
+PID=$(pgrep exec.sh)
+wait_for_PID $PID
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make check" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tChecking de Protobuf $PROTOBUF_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --working-directory $PWD --title="LABSIM - Protobuf $PROTOBUF_INSTALL_TARGET_VERSION" --command "./exec.sh" --window
+sleep 0.2
+PID=$(pgrep exec.sh)
+wait_for_PID $PID
+echo "OK"
+
+echo "#!/bin/bash" > exec.sh
+echo "make install" >> exec.sh
+echo "read -p \"Appuyez sur [Entree] pour continuer...\"" >> exec.sh
+echo -ne "\tChecking de Protobuf $PROTOBUF_INSTALL_TARGET_VERSION ... "
+chmod u+x exec.sh
+gnome-terminal --working-directory $PWD --title="LABSIM - Protobuf $PROTOBUF_INSTALL_TARGET_VERSION" --command "./exec.sh" --window
+sleep 0.2
+PID=$(pgrep exec.sh)
+wait_for_PID $PID
+echo "OK"
+
+echo "########################################################"
+echo "  Suppression du repertoire temporaire"
+cd $current_dir
+rm -rf /tmp_user/$(hostname)/$(whoami)/protobufinst
+echo "########################################################"
+
+echo; echo "## END"; echo
+
